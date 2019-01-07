4f8f6da5b76b0cebc585433218fab19aa04c5465
AlekTutchton <atutchton@email.arizona.edu>
2019-01-06 18:43:03 -0700

    pipiline

--- truffle-local.js
+++ truffle-local.js
@@ -1,8 +1,13 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
       port: 8545,
+      network_id: "*"
+    }
+    buddy: {
+      host: "ganache"
+      port: 8545
       network_id: "*"
     }
   }
