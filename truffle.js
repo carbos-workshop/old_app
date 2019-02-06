997046f2b8c7818a06f9ca77ee843b9975d918e5
Nathan Tutchton <ntutchton@gmail.com>
2019-02-05 19:39:43 -0700

    rinkleby stuff

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
+const HDWallerProvider = require('truffle-hdwallet-provider')
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: function(){
+        return new HDWallerProvider( process.env.INFURA_MNEMONIC, process.env.INFURA_API )
+      },
+      network_id: 4,
     }
   }
 };
