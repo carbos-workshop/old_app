706bf3b8e8c8405b4627588b9d6796fbe4c9aff4
Nathan Tutchton <ntutchton@gmail.com>
2019-02-05 21:04:36 -0700

    wip

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -4,5 +4,7 @@ import Web3 from 'web3'
 export const uport = new Connect('Carbos')
 const provider = uport.getProvider()
 export const web3 = new Web3(provider)
+console.log('uport', uport);
+console.log('web3', web3);
 
 // export const web3 = new Web3(Web3.givenProvider || "ws://localhost:8546")
--- src/util/utils.js
+++ src/util/utils.js
@@ -1,12 +1,15 @@
+import { web3 } from './connectors'
+
 export function trimDecimals(number, length) {
   return Number.parseFloat(number).toFixed((length || 2))
 }
 
 export function createTransactionObject(){
     return {
-      to: '0xc3245e75d3ecd1e81a9bfb6558b6dafe71e9f347',
-      value: '0.069',
-      fn: "setStatus(string 'Testy McTestFace', bytes32 '0xc3245e75d3ecd1e81a9bfb6558b6dafe71e9f347')",
+      // from: 'did:ethr:0x60ffc1a9ed87bd44cd5c1bf43b16e7a7a70c0de9',
+      to: '0x7bb53B80ccA74eEC085c93995Fb72a7057bb53aa',//'0xc3245e75d3ecd1e81a9bfb6558b6dafe71e9f347',
+      value: web3.utils.toWei('1', 'ether'),
+      net: '0x4',
       appName: 'Carbos'
     }
 }
