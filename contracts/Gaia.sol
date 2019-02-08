a2ebfbb134066fc4c37bb0b531a1d2fe97c6dbf0
Nathan Tutchton <ntutchton@gmail.com>
2019-02-07 20:32:23 -0700

    added Gaia & C3

---
+++ contracts/Gaia.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.3;
+
+contract Gaia {
+       // function sendDeposit() public gaiaOnly { GAIA JUST TRANSFERS BALNCE TO HERE WITH CONSTRUCTION
+        //only gaia can hold funds in escrow
+        //define payee/submitter
+        //get carbon value from c3, take 5%
+        // int carbon = 20854612619117486000; //TEMP HARDCODE
+        // carbon = carbon/20;
+        // //get carbon -> eth price from oracle (price per ton in eth)
+        // int ppt = 500000000000000000; //(.5 ether in Wei)vTEMP HARDCODE
+    //}
+}
