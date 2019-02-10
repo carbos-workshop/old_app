623931dd6df976efca9b3b395b0b52df617dff37
AlekTutchton <atutchton@email.arizona.edu>
2019-02-09 22:32:02 -0700

    no more endorsed

--- contracts/Gaia.sol
+++ contracts/Gaia.sol
@@ -30,10 +30,8 @@ contract Gaia {
     mapping(address => User) public users;
 
     //stores all the c3s addresses to their corrispoding User
-    mapping(address => address) public pending_c3;
+    mapping(address => address) public c3s;
 
-    //may want this later, right now it is just stored in the allC3s
-    mapping(address => address) public endorsed_c3;
 
 
 
@@ -62,7 +60,7 @@ contract Gaia {
     }
 
     /*
-    * @function: adds a newly created C3 to a pending_c3 mapping until it is endorsed.
+    * @function: adds a newly created C3 to a c3s mapping until it is endorsed.
     * @param userAddress: payable? address of the contract owner
     * @param: TODO add remaining c3 params
     */
@@ -101,8 +99,9 @@ contract Gaia {
           // carbos
         );
         //TODO reject c3s that have a identical geometryHash
-        pending_c3[address(newC3)] = msg.sender;
+        c3s[address(newC3)] = msg.sender;
         users[msg.sender].c3s.push(address(newC3));
+        allC3s.push(address(newC3));
 
         //create escrow and transfer value
         Escrow newEscrow = (new Escrow).value(msg.value)(msg.sender, address(endorser), carbos);
@@ -121,28 +120,4 @@ contract Gaia {
         return users[userAddress].c3s;
     }
 
-
-    /*
-      @function adds an endorsed contract to the mapping, deletes from the pending_c3
-    * and adds to the allC3 array.
-    * @param c3Contract: address of the C3 contract
-    * @param userAddress: address of the user that created the C3
-    */
-    function nowEndorsed(address c3Contract, address userAddress) public {
-      require(msg.sender == address(endorser));
-      endorsed_c3[c3Contract] = userAddress;
-      allC3s.push(c3Contract);
-      delete pending_c3[c3Contract];
-    }
-
-
-       // function sendDeposit() public gaiaOnly { GAIA JUST TRANSFERS BALNCE TO HERE WITH CONSTRUCTION
-        //only gaia can hold funds in escrow
-        //define payee/submitter
-        //get carbon value from c3, take 5%
-        // int carbon = 20854612619117486000; //TEMP HARDCODE
-        // carbon = carbon/20;
-        // //get carbon -> eth price from oracle (price per ton in eth)
-        // int ppt = 500000000000000000; //(.5 ether in Wei)vTEMP HARDCODE
-    //}
 }
